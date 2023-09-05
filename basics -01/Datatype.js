14ab46e5fba8e8e4d1894ea4d7b731103a7fb034
ajaythopate <ajaythopate999@gmail.com>
2023-09-05 14:58:56 +0530

    datatype  added

---
+++ basics -01/Datatype.js
@@ -0,0 +1,25 @@
+// there are 6 Datatype 
+
+// Number
+// String
+// Boolean
+// BigInt
+// String
+// Symbol
+
+
+7;
+2.5;
+//Number values
+
+'Ajay';
+"Ajay";
+// String values
+
+true;
+false;
+// Boolean values
+
+null;
+undefined;
+//Empty values
