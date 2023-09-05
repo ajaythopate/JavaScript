05d2ffce61c64d2fea4582b11a493297c1e254c1
ajaythopate <ajaythopate999@gmail.com>
2023-09-05 15:44:06 +0530

    operator  added

---
+++ basics -01/Operators.js
@@ -0,0 +1,37 @@
+// Assignment operator
+
+let x = 10;
+let y = 10;
+ 
+// Arthimatic operator
+//Arthimetic operater are  +, - , * , / , % 
+
+let z = x + y;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+
+// Comparison operater 
+let abc =10;
+let xyz = 20;
+
+console.log(abc > xyz);
+console.log(abc < xyz);
+console.log(abc >= xyz);
+console.log(abc <= xyz);
+console.log(abc != xyz);
+
+
+// == or ===
+
+let acb = 10;
+let xzy = '10';
+
+console.log(acb == xzy);
+// == compaire to values not datatype
+
+console.log(acb === xzy);
+// === compaire values as well as datatype
