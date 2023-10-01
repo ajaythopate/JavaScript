f39495cc7af9c42c7d893a8abe0312a4675e5938
ajaythopate <ajaythopate999@gmail.com>
2023-10-01 12:35:47 +0530

    Object assign Method

--- basics -01/06-Object.js
+++ basics -01/06-Object.js
@@ -16,9 +16,18 @@ console.log(user["email"]) //ajaygoogle.com
 
 
 
-
 //object freeze method
 
 user.email = "ajaychatgpt.com"
 Object.freeze(user); //ajaychatgpt.com
 
+
+//Object.assign()
+
+//The Object.assign() static method copies all enumerable own properties from one or more source objects to a target object. It returns the modified target object.
+const subject = { name: "Maths" }
+
+const marks = { score: 60 }
+
+const result = Object.assign(subject, marks);
+console.log(result);
